7ce7f5e7c7d84e237d42089d3e0dc73d700b31d0
Tun1ck <listov0@bk.ru>
2021-10-28 19:23:46 +0300

    Что то сделали

---
+++ AlbinaServer1/Albina.DataAccess.Core/Models/UserRto.cs
@@ -0,0 +1,12 @@
+﻿
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Albina.DataAccess.Core.Models
+{
+    [Table("User")]
+    class UserRto
+    {
+        public int MyProperty { get; set; }
+    }
+}
