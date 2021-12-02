013f5f0915135a36cfbdc675268933f78c6e70fb
Tatyana-Essentya <listov0@bk.ru>
2021-12-02 19:29:31 +0300

    111nngfg
    111

--- AlbinaServer1/Albina.BisinesLogic/Services/UserService.cs
+++ AlbinaServer1/Albina.BisinesLogic/Services/UserService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Albina.BisinesLogic.Interfaces;
+using Albina.BisinesLogic.Models;
+using Albina.DataAccess.Core.Interfaces;
+using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,39 @@ using System.Threading.Tasks;
 
 namespace Albina.BisinesLogic.Services
 {
-    class UserService
+    public class UserService : IUserService
     {
+        private readonly IMapper _mapper;
+        private readonly IContext _context;
+
+        public UserService(IMapper mapper, IContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+        public Task<UserInformationBlo> Auth(UserIdentityBlo userIdentityBlo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DoesExist(int numberPrefix, int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserInformationBlo> Get(int userId)
+        {
+            _context.Users.Fin
+        }
+
+        public Task<UserInformationBlo> Register(UserIdentityBlo userIdentityBlo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserUpdateBlo> Update(UserIdentityBlo userIdentityBlo, UserUpdateBlo userUpdateBlo)
+        {
+            throw new NotImplementedException();
+        } 
     }
 }
